c33214e0bfcd6817e519be07d9e841c7fc17d846
Daniel Volchek <danielvolchek123@gmail.com>
2022-09-24 16:42:27 -0400

    fix assets

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,11 +1,18 @@
+import useBreakpoints from "../../hooks/useWindowDimensions";
 import styles from "./Hero.module.scss";
 import Nav from "./Nav";
 
 type Props = {};
 
 export default function Hero({}: Props) {
+	const br = useBreakpoints();
+
 	return (
-		<div className={styles.hero}>
+		<div
+			className={styles.hero}
+			style={{
+				backgroundImage: `url(images/${br}/image-hero.jpg`,
+			}}>
 			<Nav />
 			<h1>IMMERSIVE EXPERIENCES THAT DELIVER</h1>
 			<div className={styles.spacer}></div>
